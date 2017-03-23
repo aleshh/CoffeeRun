c148dae536ff0318cd4e18e43f9ce14906ce0e0f
Alesh Houdek <mail@alesh.com>
2017-03-23 11:39:57 -0600

    Ch. 10 Silver Challenge

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -28,10 +28,46 @@
       fn(data);
       this.reset();
       this.elements[0].focus();
+      $('#caffineLevelLabel').text('Strength: 30').removeClass().addClass('label label-success');
+    });
+  };
+
+  function StrengthSliderHandler(selector) {
+    if (!selector) {
+      throw new Error('No selector provided');
+    }
+
+    this.$formElement = $(selector);
+    if (this.$formElement.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
+  }
+
+  StrengthSliderHandler.prototype.addAdjustHandler = function() {
+    console.log('Adding slider adjust handler for ');
+    // console.log(this);
+    // console.log(this.$formElement);
+    this.$formElement.on('change', function(event) {
+      var sliderClass = "label ";
+
+      if (this.value <= 33) {
+        sliderClass += "label-success";
+      } else {
+        if (this.value >= 66) {
+          sliderClass += "label-danger";
+        } else {
+          sliderClass += "label-warning";
+        }
+      }
+      $('#caffineLevelLabel').text('Strength: ' + this.value)
+        .removeClass().addClass(sliderClass);
+
+      console.log('Changing slider value to ' + this.value + ', label to ' + sliderClass);
     });
   };
 
   App.FormHandler = FormHandler;
+  App.StrengthSliderHandler = StrengthSliderHandler;
   window.App = App;
 
 })(window);
--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,18 @@
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
+  var StrengthSliderHandler = App.StrengthSliderHandler;
 
   var myTruck = new Truck('ncc-1701', new DataStore());
 
   window.myTruck = myTruck;
-  var formHandler = new FormHandler(FORM_SELECTOR);
 
+  var formHandler = new FormHandler(FORM_SELECTOR);
   formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
   console.log(formHandler);
+
+  var strengthSliderHandler = new StrengthSliderHandler('#strengthLevel');
+  strengthSliderHandler.addAdjustHandler();
 
   // myTruck.createOrder({ emailAddress: 'dr@no.com', coffee: 'decaf'});
   // myTruck.createOrder({ emailAddress: 'james@bond.com', coffee: 'espresso'});
