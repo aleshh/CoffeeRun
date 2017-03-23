7b05529fb028a178948add0c57c77eec1d44bb98
Alesh Houdek <mail@alesh.com>
2017-03-23 11:08:34 -0600

    End of ch. 10

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+(function (window) {
+  'use strict';
+  var FORM_SELECTOR = '[data-coffee-order="form"]';
+  var App = window.App;
+  var Truck = App.Truck;
+  var DataStore = App.DataStore;
+  var FormHandler = App.FormHandler;
+
+  var myTruck = new Truck('ncc-1701', new DataStore());
+
+  window.myTruck = myTruck;
+  var formHandler = new FormHandler(FORM_SELECTOR);
+
+  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  console.log(formHandler);
+
+  // myTruck.createOrder({ emailAddress: 'dr@no.com', coffee: 'decaf'});
+  // myTruck.createOrder({ emailAddress: 'james@bond.com', coffee: 'espresso'});
+  // myTruck.createOrder({ emailAddress: 'q@bond.com', coffee: 'tea'});
+  // myTruck.printOrders();
+  // myTruck.deliverOrder('dr@no.com');
+  // myTruck.printOrders();
+
+})(window);
