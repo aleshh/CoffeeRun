dfc52e7fdbe59fe417fc856d1e2353db0020da7a
Alesh Houdek <mail@alesh.com>
2017-03-25 11:54:20 -0600

    Added click handler for pending orders

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -15,10 +15,25 @@
     }
   }
 
+  CheckList.prototype.addClickHandler = function (fn) {
+    this.$element.on('click', 'input', function(event) {
+      var email = event.target.value;
+      this.removeRow(email);
+      fn(email);
+    }.bind(this));
+  };
+
   CheckList.prototype.addRow = function(coffeeOrder) {
+    this.removeRow(coffeeOrder.emailAddress);
     var rowElement = new Row(coffeeOrder);
 
     this.$element.append(rowElement.$element);
+  };
+
+  CheckList.prototype.removeRow = function(email) {
+    this.$element.find('[value = "' + email + '"]')
+    .closest('[data-coffee-order="checkbox"]')
+    .remove();
   };
 
   function Row(coffeeOrder) {
